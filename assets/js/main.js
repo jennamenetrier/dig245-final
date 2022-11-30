3ab8812286d21b7d43c9c7ebf13ed6cc82cef12d
Jenna <jemenetrier@davidson.edu>
2022-11-30 00:14:22 -0500

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,43 @@
 
 /* javascript */
+const quizAnswer =  ["a", 'b', 'a', 'b', 'b', 'a', 'b', 'a', 'b']
+const userAnswer = []
+
+document.getElementById("button-0-a").addEventListener("click", function(){
+   userAnswer.push("a")
+  if (userAnswer[0]===quizAnswer[0]) {
+    document.getElementById("button-0-a").style.backgroundColor="Green"
+  } else{
+    document.getElementById("button-0-a").style.backgroundColor="Red"
+  }
+
+});
+document.getElementById("button-0-b").addEventListener("click", function(){
+   userAnswer.push("b")
+  if (userAnswer[0]===quizAnswer[0]) {
+    document.getElementById("button-0-b").style.backgroundColor="green"
+  } else{
+    document.getElementById("button-0-b").style.backgroundColor="red"
+  }
+
+});
+
+
+document.getElementById("button-1-a").addEventListener("click", function(){
+   userAnswer.push("a")
+  if (userAnswer[1]===quizAnswer[1]) {
+    document.getElementById("button-1-a").style.backgroundColor="Green"
+  } else{
+    document.getElementById("button-1-a").style.backgroundColor="Red"
+  }
+
+});
+document.getElementById("button-1-b").addEventListener("click", function(){
+   userAnswer.push("b")
+  if (userAnswer[1]===quizAnswer[1]) {
+    document.getElementById("button-1-b").style.backgroundColor="green"
+  } else{
+    document.getElementById("button-1-b").style.backgroundColor="red"
+  }
+
+});
