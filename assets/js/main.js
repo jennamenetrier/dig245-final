8c79360389c004be7cf5158c7de26d14af5feabf
Jenna <jemenetrier@davidson.edu>
2022-11-30 10:33:30 -0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,187 @@
 
 /* javascript */
-const quizAnswer =  ["a", 'b', 'a', 'b', 'b', 'a', 'b', 'a', 'b']
-const userAnswer = []
+const quizAnswer =  ["a", 'b', 'a', 'b', 'b', 'a', 'b', 'a', 'b', 'b'];
+const userAnswer = [];
 
 document.getElementById("button-0-a").addEventListener("click", function(){
-   userAnswer.push("a")
+   userAnswer.push("a");
   if (userAnswer[0]===quizAnswer[0]) {
-    document.getElementById("button-0-a").style.backgroundColor="Green"
+    document.getElementById("button-0-a").style.backgroundColor="Green";
   } else{
-    document.getElementById("button-0-a").style.backgroundColor="Red"
+    document.getElementById("button-0-a").style.backgroundColor="Red";
   }
 
 });
 document.getElementById("button-0-b").addEventListener("click", function(){
-   userAnswer.push("b")
+   userAnswer.push("b");
   if (userAnswer[0]===quizAnswer[0]) {
-    document.getElementById("button-0-b").style.backgroundColor="green"
+    document.getElementById("button-0-b").style.backgroundColor="green";
   } else{
-    document.getElementById("button-0-b").style.backgroundColor="red"
+    document.getElementById("button-0-b").style.backgroundColor="red";
   }
 
 });
 
 
 document.getElementById("button-1-a").addEventListener("click", function(){
-   userAnswer.push("a")
+   userAnswer.push("a");
   if (userAnswer[1]===quizAnswer[1]) {
-    document.getElementById("button-1-a").style.backgroundColor="Green"
+    document.getElementById("button-1-a").style.backgroundColor="Green";
   } else{
-    document.getElementById("button-1-a").style.backgroundColor="Red"
+    document.getElementById("button-1-a").style.backgroundColor="Red";
   }
 
 });
 document.getElementById("button-1-b").addEventListener("click", function(){
-   userAnswer.push("b")
+   userAnswer.push("b");
   if (userAnswer[1]===quizAnswer[1]) {
-    document.getElementById("button-1-b").style.backgroundColor="green"
+    document.getElementById("button-1-b").style.backgroundColor="green";
   } else{
-    document.getElementById("button-1-b").style.backgroundColor="red"
+    document.getElementById("button-1-b").style.backgroundColor="red";
+  }
+
+});
+document.getElementById("button-2-a").addEventListener("click", function(){
+   userAnswer.push("a");
+  if (userAnswer[2]===quizAnswer[2]) {
+    document.getElementById("button-2-a").style.backgroundColor="Green";
+  } else{
+    document.getElementById("button-2-a").style.backgroundColor="Red";
+  }
+
+});
+document.getElementById("button-2-b").addEventListener("click", function(){
+   userAnswer.push("b");
+  if (userAnswer[2]===quizAnswer[2]) {
+    document.getElementById("button-2-b").style.backgroundColor="green";
+  } else{
+    document.getElementById("button-2-b").style.backgroundColor="red";
+  }
+
+});
+document.getElementById("button-3-a").addEventListener("click", function(){
+   userAnswer.push("a");
+  if (userAnswer[3]===quizAnswer[3]) {
+    document.getElementById("button-3-a").style.backgroundColor="Green";
+  } else{
+    document.getElementById("button-3-a").style.backgroundColor="Red";
+  }
+
+});
+document.getElementById("button-3-b").addEventListener("click", function(){
+   userAnswer.push("b");
+  if (userAnswer[3]===quizAnswer[3]) {
+    document.getElementById("button-3-b").style.backgroundColor="green";
+  } else{
+    document.getElementById("button-3-b").style.backgroundColor="red";
+  }
+
+});
+document.getElementById("button-4-a").addEventListener("click", function(){
+   userAnswer.push("a");
+  if (userAnswer[4]===quizAnswer[4]) {
+    document.getElementById("button-4-a").style.backgroundColor="Green";
+  } else{
+    document.getElementById("button-4-a").style.backgroundColor="Red";
+  }
+
+});
+document.getElementById("button-4-b").addEventListener("click", function(){
+   userAnswer.push("b");
+  if (userAnswer[4]===quizAnswer[4]) {
+    document.getElementById("button-4-b").style.backgroundColor="green";
+  } else{
+    document.getElementById("button-4-b").style.backgroundColor="red";
+  }
+
+});
+document.getElementById("button-5-a").addEventListener("click", function(){
+   userAnswer.push("a");
+  if (userAnswer[5]===quizAnswer[5]) {
+    document.getElementById("button-5-a").style.backgroundColor="Green";
+  } else{
+    document.getElementById("button-5-a").style.backgroundColor="Red";
+  }
+
+});
+document.getElementById("button-5-b").addEventListener("click", function(){
+   userAnswer.push("b");
+  if (userAnswer[5]===quizAnswer[5]) {
+    document.getElementById("button-5-b").style.backgroundColor="green";
+  } else{
+    document.getElementById("button-5-b").style.backgroundColor="red";
+  }
+
+});
+document.getElementById("button-6-a").addEventListener("click", function(){
+   userAnswer.push("a");
+  if (userAnswer[6]===quizAnswer[6]) {
+    document.getElementById("button-6-a").style.backgroundColor="Green";
+  } else{
+    document.getElementById("button-6-a").style.backgroundColor="Red";
+  }
+
+});
+document.getElementById("button-6-b").addEventListener("click", function(){
+   userAnswer.push("b");
+  if (userAnswer[6]===quizAnswer[6]) {
+    document.getElementById("button-6-b").style.backgroundColor="green";
+  } else{
+    document.getElementById("button-6-b").style.backgroundColor="red";
+  }
+
+});
+document.getElementById("button-7-a").addEventListener("click", function(){
+   userAnswer.push("a");
+  if (userAnswer[7]===quizAnswer[7]) {
+    document.getElementById("button-7-a").style.backgroundColor="Green";
+  } else{
+    document.getElementById("button-7-a").style.backgroundColor="Red";
+  }
+
+});
+document.getElementById("button-7-b").addEventListener("click", function(){
+   userAnswer.push("b");
+  if (userAnswer[7]===quizAnswer[7]) {
+    document.getElementById("button-7-b").style.backgroundColor="green";
+  } else{
+    document.getElementById("button-7-b").style.backgroundColor="red";
+  }
+
+});
+document.getElementById("button-8-a").addEventListener("click", function(){
+   userAnswer.push("a");
+  if (userAnswer[8]===quizAnswer[8]) {
+    document.getElementById("button-8-a").style.backgroundColor="Green";
+  } else{
+    document.getElementById("button-8-a").style.backgroundColor="Red";
+  }
+
+});
+document.getElementById("button-8-b").addEventListener("click", function(){
+   userAnswer.push("b");
+  if (userAnswer[8]===quizAnswer[8]) {
+    document.getElementById("button-8-b").style.backgroundColor="green";
+  } else{
+    document.getElementById("button-8-b").style.backgroundColor="red";
+  }
+
+});
+document.getElementById("button-9-a").addEventListener("click", function(){
+   userAnswer.push("a");
+  if (userAnswer[9]===quizAnswer[9]) {
+    document.getElementById("button-9-a").style.backgroundColor="Green";
+  } else{
+    document.getElementById("button-9-a").style.backgroundColor="Red";
+  }
+
+});
+document.getElementById("button-9-b").addEventListener("click", function(){
+   userAnswer.push("b");
+  if (userAnswer[9]===quizAnswer[9]) {
+    document.getElementById("button-9-b").style.backgroundColor="green";
+  } else{
+    document.getElementById("button-9-b").style.backgroundColor="red";
   }
 
 });
